e2b92fd9ff282a3cdfddf6b0fb0b620dc44429ed
Bertrand Darbon <bertrand@hyle.eu>
2025-02-21 12:15:41 +0100

    wipe

--- dist/src/ByteArray.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @param data Decimal encoded number as a string
- * @returns
- */
-export declare function deserByteArray(data: (bigint | string | number)[]): string;
-export type ByteArray = string;
-export declare function serByteArray(arr: ByteArray): string;
-//# sourceMappingURL=ByteArray.d.ts.map
--- dist/src/network.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function persistentRef<T>(key: string, initialValue: T): [T] extends [import("@vue/reactivity").Ref<any, any>] ? import("@vue/shared").IfAny<T, import("@vue/reactivity").Ref<T, T>, T> : import("@vue/reactivity").Ref<import("@vue/reactivity").UnwrapRef<T>, T | import("@vue/reactivity").UnwrapRef<T>>;
-export declare const getNetworkApiUrl: (network: string) => string | undefined;
-export declare const getNetworkWebsocketUrl: (network: string) => string | undefined;
-//# sourceMappingURL=network.d.ts.map
