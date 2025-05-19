4d5c3a4e6d791542146efe37fcfae058c3b4971e
Bertrand Darbon <bertrand@hyle.eu>
2025-05-19 16:32:59 +0200

    ✨ Add smt token

--- src/blob_builder.ts
+++ src/blob_builder.ts
@@ -1,9 +1,11 @@
 import * as token from "./model/token";
 import * as metamask from "./model/mmid";
 import * as amm from "./model/amm";
+import * as smt_token from "./model/smt_token";
 
 export const blob_builder = {
     token,
+    smt_token,
     metamask,
     amm,
 };
